30ab3b5fea58f34179b9b258738f337469b477aa
cjherm <akbigchris@gmail.com>
2025-10-21 09:25:54 +0200

    Added new arguments for init, diff and restore.

--- app/src/main/kotlin/org/example/BackMeUpApplication.kt
+++ app/src/main/kotlin/org/example/BackMeUpApplication.kt
@@ -25,8 +25,36 @@ class BackMeUpApplication {
         }
     }
 
+    /*
+    Arguments hierarchy:
+        -init       (creates first full backup)
+        |___-save       (directory to be backed up)
+        |___-storage    (directory to store the backup)
+        -diff       (creates diff from specified backup)
+        |___-src        (directory to compare)
+        |___-storage    (optional, directory to store the diff backup)
+        -restore    (restores complete file system from a backup)
+        |___-from       (last state of backup to restore from)
+        |___-to         (directory in which to restore)
+ */
     companion object {
-        const val ARG_INIT = "init"
         const val ARG_MARKER = "-"
+        const val ARG_INIT = "init"
+        const val ARG_DIFF = "diff"
+        const val ARG_RESTORE = "restore"
+        const val ARG_SAVE = "save"
+        const val ARG_STORAGE = "storage"
+        const val ARG_SRC = "src"
+        const val ARG_FROM = "from"
+        const val ARG_TO = "to"
+
+        val TOP_ARGS = arrayOf(argOf(ARG_INIT), argOf(ARG_DIFF), argOf(ARG_RESTORE))
+        val INIT_ARGS = arrayOf(argOf(ARG_SAVE), argOf(ARG_STORAGE))
+        val DIFF_ARGS = arrayOf(argOf(ARG_SRC), argOf(ARG_STORAGE))
+        val RESTORE_ARGS = arrayOf(argOf(ARG_FROM), argOf(ARG_TO))
+
+        private fun argOf(arg: String): String {
+            return ARG_MARKER + arg
+        }
     }
 }
