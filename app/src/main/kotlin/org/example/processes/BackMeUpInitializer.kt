98f2c4aff5a8e7e39eeb223c83eb9952b2356f84
cjherm <akbigchris@gmail.com>
2025-10-21 18:16:45 +0200

    Added another missing import statement.

--- app/src/main/kotlin/org/example/processes/BackMeUpInitializer.kt
+++ app/src/main/kotlin/org/example/processes/BackMeUpInitializer.kt
@@ -1,5 +1,6 @@
 package org.example.processes
 
+import org.example.BackMeUpResult
 import org.example.configs.InitProcessConfig
 
 class BackMeUpInitializer(val config: InitProcessConfig) : BackMeUpProcess {
