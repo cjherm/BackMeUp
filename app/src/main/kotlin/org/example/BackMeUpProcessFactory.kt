ae533f68561ddaad0a52aaa879c65077e1cd0fef
cjherm <akbigchris@gmail.com>
2025-10-21 11:21:42 +0200

    Reworked process factory.

--- app/src/main/kotlin/org/example/BackMeUpProcessFactory.kt
+++ app/src/main/kotlin/org/example/BackMeUpProcessFactory.kt
@@ -5,7 +5,7 @@ class BackMeUpProcessFactory(val rawArgs: Array<String>) {
     fun createProcess(): BackMeUpProcess? {
 
         if (containsNoOrMoreThanOneTopArg(rawArgs.toList())) {
-            println("Must contain exact one of these top arguments:\n\t-init\nt\t-diff\n\t-restore!")
+            println("Must contain exact ONE of these top arguments:\n\t-init\nt\t-diff\n\t-restore!")
             return null
         }
 
@@ -28,48 +28,98 @@ class BackMeUpProcessFactory(val rawArgs: Array<String>) {
         return count != 1
     }
 
-    private fun containsNotAtLeastOneOfEach(argsToCheck: List<String>, mustBeArgs: Array<String>): Boolean {
-        return mustBeArgs.any { it !in argsToCheck }
+    private fun retrieveArgValuesFromArgs(argKey: String, argsToCheck: List<String>): List<String> {
+        val argValues = mutableListOf<String>()
+        argsToCheck.forEachIndexed { index, value ->
+            if (value == argKey && argsToCheck.size >= index + 2) {
+                argValues.add(argsToCheck[index + 1])
+            }
+        }
+        return argValues
     }
 
     private fun checkInitArgsAndCreateProcess(givenArgs: List<String>): BackMeUpProcess? {
-        if (containsNotAtLeastOneOfEach(givenArgs, INIT_ARGS)) {
+        val saveValuesList = retrieveArgValuesFromArgs(argOf(ARG_SAVE), givenArgs)
+
+        if (saveValuesList.isEmpty()) {
+            println("No values for \"-save\" found in $givenArgs!")
             return null
         }
-        return BackMeUpInitializer(givenArgs.toList())
+
+        val storageValuesList = retrieveArgValuesFromArgs(argOf(ARG_STORAGE), givenArgs)
+
+        if (storageValuesList.isEmpty()) {
+            println("No values for \"-storage\" found in $givenArgs!")
+            return null
+        }
+
+        return BackMeUpInitializer(InitProcessConfig())
     }
 
     private fun checkDiffArgsAndCreateProcess(givenArgs: List<String>): BackMeUpProcess? {
-        if (containsNotAtLeastOneOfEach(givenArgs, arrayOf(argOf(ARG_SRC)))) {
+        val srcValuesList = retrieveArgValuesFromArgs(argOf(ARG_SRC), givenArgs)
+
+        if (srcValuesList.isEmpty()) {
+            println("No values for \"-src\" found in $givenArgs!")
             return null
         }
-        return BackMeUpDifferentiator(givenArgs.toList())
+
+        if (srcValuesList.size > 1) {
+            println("More than one value found for \"-src\" in $givenArgs!")
+            return null
+        }
+
+        val storageValuesList = retrieveArgValuesFromArgs(argOf(ARG_STORAGE), givenArgs)
+
+        if (storageValuesList.isEmpty()) {
+            // TODO Adapt this output
+            println("No values for \"-storage\" found in $givenArgs! Will use this storage: \"\"")
+        }
+
+        return BackMeUpDifferentiator(DiffProcessConfig())
     }
 
     private fun checkRestoreArgsAndCreateProcess(givenArgs: List<String>): BackMeUpProcess? {
-        if (containsNotAtLeastOneOfEach(givenArgs, RESTORE_ARGS)) {
+        val fromValuesList = retrieveArgValuesFromArgs(argOf(ARG_FROM), givenArgs)
+
+        if (fromValuesList.isEmpty()) {
+            println("No values for \"-from\" found in $givenArgs!")
             return null
         }
-        return BackMeUpRestorer(givenArgs.toList())
+
+        if (fromValuesList.size > 1) {
+            println("More than one value found for \"-from\" in $givenArgs!")
+            return null
+        }
+
+        val toValuesList = retrieveArgValuesFromArgs(argOf(ARG_TO), givenArgs)
+
+        if (toValuesList.isEmpty()) {
+            println("No values for \"-to\" found in $givenArgs!")
+            return null
+        }
+
+        return BackMeUpRestorer(RestoreProcessConfig())
     }
 
     /*
     Arguments hierarchy:
 
     -init       (creates first full backup)
-    |___-save       (directory to be backed up)
-    |___-storage    (directory to store the backup)
+    |___-save       (multiple directories to be backed up)
+    |___-storage    (multiple directories to store the backup)
     -diff       (creates diff from specified backup)
-    |___-src        (directory to compare)
-    |___-storage    (optional, directory to store the diff backup)
+    |___-src        (single directory to compare to)
+    |___-storage    (optional, multiple directories to store the diff backup)
     -restore    (restores complete file system from a backup)
-    |___-from       (last state of backup to restore from)
-    |___-to         (directory in which to restore)
+    |___-from       (single last state of backup to restore from)
+    |___-to         (multiple directories in which to restore)
 
     Note: Only one top level argument per execution allowed
  */
     companion object {
         const val ARG_MARKER = "-"
+
         const val ARG_INIT = "init"
         const val ARG_DIFF = "diff"
         const val ARG_RESTORE = "restore"
@@ -80,9 +130,6 @@ class BackMeUpProcessFactory(val rawArgs: Array<String>) {
         const val ARG_TO = "to"
 
         val TOP_ARGS = arrayOf(argOf(ARG_INIT), argOf(ARG_DIFF), argOf(ARG_RESTORE))
-        val INIT_ARGS = arrayOf(argOf(ARG_SAVE), argOf(ARG_STORAGE))
-        val DIFF_ARGS = arrayOf(argOf(ARG_SRC), argOf(ARG_STORAGE))
-        val RESTORE_ARGS = arrayOf(argOf(ARG_FROM), argOf(ARG_TO))
 
         private fun argOf(arg: String): String {
             return ARG_MARKER + arg
