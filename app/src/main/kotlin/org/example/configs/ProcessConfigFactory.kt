2a150e13f710eaa7f360281030ebbe8f07f92f19
cjherm <akbigchris@gmail.com>
2025-10-22 13:43:20 +0200

    Reworked config factory a little bit to allow returning null instead of configs.

--- app/src/main/kotlin/org/example/configs/ProcessConfigFactory.kt
+++ app/src/main/kotlin/org/example/configs/ProcessConfigFactory.kt
@@ -2,29 +2,59 @@ package org.example.configs
 
 import org.example.processes.BackMeUpProcess.ProcessType
 
+// TODO Move some checks from process factory into this class to avoid double checks
 class ProcessConfigFactory(private val processType: ProcessType) {
 
-    fun createConfig(): BackMeUpProcessConfig {
+    var saveValuesList = emptyList<String>()
+    var storageValuesList = emptyList<String>()
+    var srcValuesList = emptyList<String>()
+    var fromValuesList = emptyList<String>()
+    var toValuesList = emptyList<String>()
+
+    fun createConfig(): BackMeUpProcessConfig? {
 
         return when (processType) {
 
             ProcessType.INIT -> {
-                InitProcessConfig(saveValuesList, storageValuesList)
+                tryToCreateInitProcessCfg()
             }
 
             ProcessType.DIFF -> {
-                DiffProcessConfig(srcValuesList, storageValuesList)
+                tryToCreateDiffProcessCfg()
             }
 
             ProcessType.RESTORE -> {
-                RestoreProcessConfig(fromValuesList, toValuesList)
+                tryToCreateRestoreProcessCfg()
             }
         }
     }
 
-    lateinit var saveValuesList: List<String>
-    lateinit var storageValuesList: List<String>
-    lateinit var srcValuesList: List<String>
-    lateinit var fromValuesList: List<String>
-    lateinit var toValuesList: List<String>
+    private fun tryToCreateInitProcessCfg(): InitProcessConfig? {
+        if (saveValuesList.isEmpty() || storageValuesList.isEmpty()) {
+            return null
+        }
+        return InitProcessConfig(saveValuesList, storageValuesList)
+    }
+
+    private fun tryToCreateDiffProcessCfg(): DiffProcessConfig? {
+        if (storageValuesList.isEmpty()) {
+            storageValuesList = tryToFindStorage()
+        }
+        if (srcValuesList.isEmpty() || storageValuesList.isEmpty()) {
+            return null
+        }
+        return DiffProcessConfig(srcValuesList, storageValuesList)
+    }
+
+    private fun tryToFindStorage(): List<String> {
+        // TODO Try to determine storage based on known init/diffs
+        return listOf("")
+    }
+
+    private fun tryToCreateRestoreProcessCfg(): RestoreProcessConfig? {
+        if (fromValuesList.isEmpty() || toValuesList.isEmpty()) {
+            return null
+        }
+        return RestoreProcessConfig(fromValuesList, toValuesList)
+    }
 }
