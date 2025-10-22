4b1b858c792a2acd9823010351da15c4d1cacdff
cjherm <akbigchris@gmail.com>
2025-10-22 14:08:37 +0200

    Added more properties to InitProcessConfig.

--- app/src/main/kotlin/org/example/configs/InitProcessConfig.kt
+++ app/src/main/kotlin/org/example/configs/InitProcessConfig.kt
@@ -3,6 +3,11 @@ package org.example.configs
 class InitProcessConfig(private val saveList: List<String>, private val storageList: List<String>) :
     BackMeUpProcessConfig {
 
+    var totalFilesCountToBeBackedUp: Int = 0
+    var totalFilesSizeToBeBackedUp: Long = 0
+    var availableSpaceList = mutableMapOf<String, Long>()
+    var listOfUnsuitableStorageDirs = mutableListOf<String>()
+
     fun getListOfDirectoriesToBackUp(): List<String> {
         return saveList
     }
