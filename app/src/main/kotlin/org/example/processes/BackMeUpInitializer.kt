65b1c90862e1ac8b55b1a4eee6ce19dfa6f0103d
cjherm <akbigchris@gmail.com>
2025-10-22 14:09:11 +0200

    Added calculation of backup and storage sizes.

--- app/src/main/kotlin/org/example/processes/BackMeUpInitializer.kt
+++ app/src/main/kotlin/org/example/processes/BackMeUpInitializer.kt
@@ -2,6 +2,7 @@ package org.example.processes
 
 import org.example.BackMeUpResult
 import org.example.configs.InitProcessConfig
+import java.io.File
 
 /**
  * The process that executes a full initial backup. Serves as the base for all future differential backups
@@ -9,9 +10,38 @@ import org.example.configs.InitProcessConfig
  */
 class BackMeUpInitializer(private val config: InitProcessConfig) : BackMeUpProcess {
 
-    // TODO implement this BackMeUpProcess
-
     override fun start(): BackMeUpResult {
+        calculateBackUpSize()
+        checkAndSaveAvailableDiskSpace()
         return BackMeUpResult.ERROR
+    }
+
+    private fun calculateBackUpSize() {
+        config.getListOfDirectoriesToBackUp().forEach { path ->
+            countFilesAndSize(path)
+        }
+    }
+
+    private fun checkAndSaveAvailableDiskSpace() {
+        val listOfUnsuitableStorageDirs = mutableListOf<String>()
+        config.getListOfDirectoriesToStoreBackUps().forEach { path ->
+            val space = File(path).usableSpace
+            config.availableSpaceList[path] = space
+            if (space < config.totalFilesSizeToBeBackedUp) {
+                listOfUnsuitableStorageDirs.add(path)
+            }
+        }
+        config.listOfUnsuitableStorageDirs = listOfUnsuitableStorageDirs
+    }
+
+    private fun countFilesAndSize(dirPath: String) {
+        val dir = File(dirPath)
+        val allFiles = dir.walkTopDown().filter { it.isFile }.toList()
+
+        val fileCount = allFiles.size
+        config.totalFilesCountToBeBackedUp += fileCount
+
+        val totalSize = allFiles.sumOf { it.length() }
+        config.totalFilesSizeToBeBackedUp += totalSize
     }
 }
