b070d1c08e456fc08d317c634a2151668e001d7b
cjherm <akbigchris@gmail.com>
2025-10-22 12:10:47 +0200

    Moved BackMeUpProcessFactoryTest to the proper package.

--- app/src/test/kotlin/org/example/processes/BackMeUpProcessFactoryTest.kt
+++ app/src/test/kotlin/org/example/processes/BackMeUpProcessFactoryTest.kt
@@ -1,12 +1,8 @@
-package org.example
+package org.example.processes
 
 import org.example.configs.DiffProcessConfig
 import org.example.configs.InitProcessConfig
 import org.example.configs.RestoreProcessConfig
-import org.example.processes.BackMeUpDifferentiator
-import org.example.processes.BackMeUpInitializer
-import org.example.processes.BackMeUpProcessFactory
-import org.example.processes.BackMeUpRestorer
 import org.junit.jupiter.api.assertNotNull
 import org.junit.jupiter.api.assertNull
 import kotlin.test.Test
